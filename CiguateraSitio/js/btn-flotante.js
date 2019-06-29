c9adcfffc0b80f0df6a5fbf63ef30c05f4eb0c3e
AlexVD1 <51519938+AlexVD1@users.noreply.github.com>
2019-06-29 16:46:49 -0500

    Add files via upload

---
+++ CiguateraSitio/js/btn-flotante.js
@@ -0,0 +1,6 @@
+var btnflotante = document.getElementById('btn-flotante');
+var tablas = document.getElementById('tablas');
+
+btnflotante.addEventListener('click',function(){                        
+    tablas.classList.toggle('mostrar-flotante');
+})
